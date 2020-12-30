85afee29a569e65042328c18237f0f92cc5f95a9
nicolaserny <nicolas.erny@xcomponent.com>
2020-12-30 16:49:05 +0100

    Remove warnings (new provider definitions)

--- main.tf
+++ main.tf
@@ -1,16 +1,30 @@
+terraform {
+  required_providers {
+    archive = {
+      source = "hashicorp/archive"
+      version = "1.3.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "2.35.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "1.9.0"
+    }
+  }
+}
+
 provider "archive" {
-  version = "1.3.0"
 }
 
 provider "aws" {
-  version    = "2.35.0"
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 provider "kubernetes" {
-  version                = "1.9.0"
   host                   = var.kubernetes_host
   client_certificate     = base64decode(var.kubernetes_client_certificate)
   client_key             = base64decode(var.kubernetes_client_key)
