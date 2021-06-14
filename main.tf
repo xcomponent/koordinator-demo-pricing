c38e2a5205d1d180950e3d77d5f0781644435f2d
Marcos Almeida <marcos.almeida@xcomponent.com>
2021-06-14 18:23:51 +0200

    main.tf: load config false

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ provider "aws" {
 }
 
 provider "kubernetes" {
+  load_config_file       = false
   host                   = var.kubernetes_host
   client_certificate     = base64decode(var.kubernetes_client_certificate)
   client_key             = base64decode(var.kubernetes_client_key)
